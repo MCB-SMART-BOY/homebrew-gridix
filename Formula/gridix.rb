f5e014b097742e22b08cb9bd8af0894030a0b889
MCB-SMART-BOY <mcb2720838051@gmail.com>
2025-12-20 21:52:09 +0800

    Update to v1.0.0

--- Formula/gridix.rb
+++ Formula/gridix.rb
@@ -1,23 +1,23 @@
 class Gridix < Formula
   desc "Fast, secure, cross-platform database management tool with Helix/Vim keybindings"
   homepage "https://github.com/MCB-SMART-BOY/Gridix"
-  version "0.5.2"
+  version "1.0.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/MCB-SMART-BOY/Gridix/releases/download/v0.5.2/gridix-macos-arm64.tar.gz"
-      sha256 "c715ba07b74d3772c072915dcee7136e907df60c0943693bc57084f8e00ed6b2"
+      url "https://github.com/MCB-SMART-BOY/Gridix/releases/download/v1.0.0/gridix-macos-arm64.tar.gz"
+      sha256 "609b8aa96f6b4ccbbadd6d94892f9cf416c55b06e87f8c1b282a21d58a032b5b"
     end
     on_intel do
-      url "https://github.com/MCB-SMART-BOY/Gridix/releases/download/v0.5.2/gridix-macos-x86_64.tar.gz"
-      sha256 "435c60714571a507e99a3c311627af12af2fa94672f3883a6cf010e504736dcc"
+      url "https://github.com/MCB-SMART-BOY/Gridix/releases/download/v1.0.0/gridix-macos-arm64.tar.gz"
+      sha256 "609b8aa96f6b4ccbbadd6d94892f9cf416c55b06e87f8c1b282a21d58a032b5b"
     end
   end
 
   on_linux do
-    url "https://github.com/MCB-SMART-BOY/Gridix/releases/download/v0.5.2/gridix-linux-x86_64.tar.gz"
-    sha256 "5076f24dabde987c6ab0b76f314affa7e8ba1b415eab5b497d665f35c393171b"
+    url "https://github.com/MCB-SMART-BOY/Gridix/releases/download/v1.0.0/gridix-linux-x86_64.tar.gz"
+    sha256 "cf97bba99a6d7e388204fb8d23086433e3df459e16673105791f7502b6d23995"
   end
 
   def install
