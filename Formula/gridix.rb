ddb3328bb344cd878a93c9ed74afac7270ea36bc
MCB-SMART-BOY <2720838051@qq.com>
2026-04-07 00:35:50 +0800

    gridix v3.8.0

--- Formula/gridix.rb
+++ Formula/gridix.rb
@@ -1,24 +1,24 @@
 class Gridix < Formula
   desc "Fast, secure, cross-platform database management tool with Helix/Vim keybindings"
   homepage "https://github.com/MCB-SMART-BOY/Gridix"
-  version "3.7.1"
+  version "3.8.0"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/MCB-SMART-BOY/Gridix/releases/download/v3.7.1/gridix-macos-arm64.tar.gz"
-      sha256 "5c033e620863f6eda0f0dd34887727237907c43e389b6033c8c4441945504f50"
+      url "https://github.com/MCB-SMART-BOY/Gridix/releases/download/v3.8.0/gridix-macos-arm64.tar.gz"
+      sha256 "90bc6c61abb60a6c2985c69cecc386f0fa266b7e621ef196f82448ec0deec1cd"
     end
     on_intel do
       # Intel Mac users can use ARM build with Rosetta 2
-      url "https://github.com/MCB-SMART-BOY/Gridix/releases/download/v3.7.1/gridix-macos-arm64.tar.gz"
-      sha256 "5c033e620863f6eda0f0dd34887727237907c43e389b6033c8c4441945504f50"
+      url "https://github.com/MCB-SMART-BOY/Gridix/releases/download/v3.8.0/gridix-macos-arm64.tar.gz"
+      sha256 "90bc6c61abb60a6c2985c69cecc386f0fa266b7e621ef196f82448ec0deec1cd"
     end
   end
 
   on_linux do
-    url "https://github.com/MCB-SMART-BOY/Gridix/releases/download/v3.7.1/gridix-linux-x86_64.tar.gz"
-    sha256 "2f12ab1224617ea660582a81831799b1be10de3917087884f8366c3f3cbf72b1"
+    url "https://github.com/MCB-SMART-BOY/Gridix/releases/download/v3.8.0/gridix-linux-x86_64.tar.gz"
+    sha256 "4232deb85430fa587971aec937da10ff00175a161c7cad30838f852f47dc566a"
   end
 
   def install
